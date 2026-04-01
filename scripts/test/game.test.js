9674bb8b2b46df786f84b2ecabd13ce8813190c3
Enrique2007s <enriquesmith458@gmail.com>
2026-04-01 15:01:58 -0400

    added tests for test arrays, installed jest and necessary json package

--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -2,3 +2,28 @@
  * @jest-environment jsdom
  */
 
+const { game } = require('../game')
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('./index.html', 'utf8');
+    document.body.innerHTML = fileContents;
+});
+
+describe("Game object contains correct keys", () => {
+    test("Score key exists", () => {
+        expect ('score' in game).toBe(true);
+    });
+
+    test("currentGame key exists", () => {
+        expect ('currentGame' in game).toBe(true);
+    });
+
+    test("playerMoves key exists", () => {
+        expect ('playerMoves' in game).toBe(true);
+    });
+
+    test("choices key exists", () => {
+        expect ('choices' in game).toBe(true);
+    });
+});
